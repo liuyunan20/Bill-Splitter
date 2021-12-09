9ea82cfd8b7c8e274d9a916203f2c239f63bfa2c
liuyunan <liuyunan.20@gmail.com>
2021-12-09 13:25:42 -0800

    divide the total bill

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,12 +1,21 @@
 bills = {}
 
 print('Enter the number of friends joining (including you):')
-join_num = int(input())
-if join_num <= 0:
+
+try:
+    join_num = int(input())
+    assert join_num > 0
+except ValueError:
+    print('No one is joining for the party')
+except AssertionError as err:
     print('No one is joining for the party')
 else:
     print('Enter the name of every friend (including you), each on a new line:')
     for i in range(join_num):
         bills[input()] = 0
     print()
+    print('Enter the total bill value:')
+    total_bill = int(input())
+    for key in bills:
+        bills[key] = round(total_bill / join_num, 2)
     print(bills)
--- Topics/Exception handling/Zero/main.py
+++ Topics/Exception handling/Zero/main.py
@@ -1,7 +1,7 @@
 n = int(input())
 denominator = int(input())
 try:
-    n / denominator
+    r = n / denominator
 except ZeroDivisionError:
     print("Division by zero is not supported")
 else:
