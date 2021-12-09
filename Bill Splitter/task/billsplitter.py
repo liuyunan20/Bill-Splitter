5e3631c8c9617fd6766d0a248515e90bc1773291
liuyunan <liuyunan.20@gmail.com>
2021-12-09 13:47:02 -0800

    choose a lucky one

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,7 +1,7 @@
+import random
+
 bills = {}
-
 print('Enter the number of friends joining (including you):')
-
 try:
     join_num = int(input())
     assert join_num > 0
@@ -11,11 +11,19 @@ except AssertionError as err:
     print('No one is joining for the party')
 else:
     print('Enter the name of every friend (including you), each on a new line:')
-    for i in range(join_num):
-        bills[input()] = 0
+    names = [input() for i in range(join_num)]
+    bills = dict.fromkeys(names, 0)
     print()
     print('Enter the total bill value:')
     total_bill = int(input())
+    print()
+    print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+    lucky_option = input()
+    if lucky_option == 'Yes':
+        lucky_one = random.choice(names)
+        print(f'{lucky_one} is the lucky one!')
+    else:
+        print('No one is going to be lucky')
     for key in bills:
-        bills[key] = round(total_bill / join_num, 2)
-    print(bills)
+        bills[key] = round(total_bill / (join_num - 1), 2)
+#    print(bills)
